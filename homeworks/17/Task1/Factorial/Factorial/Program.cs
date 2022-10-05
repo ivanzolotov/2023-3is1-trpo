b9656101bc3387a80e0ed928997cad6d075a14e6
vladmoshkin <90265608+1121n@users.noreply.github.com>
2022-10-05 20:17:44 +0500

    Add files via upload

---
+++ homeworks/17/Task1/Factorial/Factorial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fac();
+        }
+
+        static void Fac()
+        {
+            int number;
+            int quelse = 1;
+
+
+            Console.WriteLine("Введите число: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i < number + 1; i++)
+            {
+                quelse = quelse * i;
+
+          
+            }
+
+            Console.WriteLine("Факториал - " + quelse);
+        }
+
+
+
+      
+    }
+}
